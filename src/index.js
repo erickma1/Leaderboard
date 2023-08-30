1d807f801c337c90f8b807f5da147c995fd17b68
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-08-30 11:05:11 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 
