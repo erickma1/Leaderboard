e08e51808e2fdf5ad653fa13a954ef7f5ae7d990
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-08-30 11:05:26 +0000

    Update webpack.config.js
    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,18 @@ module.exports = {
     clean: true,
   },
 
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
   module: {
     rules: [
       {
