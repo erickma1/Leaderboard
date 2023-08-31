7266be03e5f4be766431cb4dcf41946c40540a81
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-08-31 19:34:04 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import { addScore, refreshScores } from './modules/apiCall.js';
+
+const addScoreBtn = document.querySelector('#add_btn');
+const refreshBtn = document.querySelector('#refresh-btn');
+
+addScoreBtn.addEventListener('click', addScore);
+refreshBtn.addEventListener('click', refreshScores);
+
+window.onload = () => {
+  refreshScores();
+};
